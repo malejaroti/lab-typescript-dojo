54acae70d925f239175c9b0009163e950212288d
Alejandra Rodriguez Tibana <30965660+malejaroti@users.noreply.github.com>
2025-09-24 12:23:24 +0200

    Lab completed

--- src/iteration2.ts
+++ src/iteration2.ts
@@ -2,4 +2,19 @@
 // Iteration 2 | Functions
 //
 
+function calcMultiplication( a:number, b:number): number {
+    return  a * b;
+}
 
+function isEven(n: number) : boolean {
+    return n % 2 === 0;
+}
+
+function calcArrayAverage( numbersArr: number[]): string | number {
+    if(numbersArr.length === 0){
+        return `Please provide an array with at least one element`
+    }
+    const sum = numbersArr.reduce((acc, curr) => acc+curr, 0)
+    console.log("sum: ",sum)
+    return sum/numbersArr.length
+}
--- src/iteration5.ts
+++ src/iteration5.ts
@@ -2,4 +2,30 @@
 // Bonus: Iteration 5 | Interfaces
 //
 
+interface MediaItem {
+    title: string
+    year: number
+    genres: string[]
+    rating: number
+}
 
+interface Book extends MediaItem {
+    author: string
+    numberOfPages?: number    
+}
+interface Movie extends MediaItem {
+    director: string
+    durationInMinutes?: number    
+}
+interface Album extends MediaItem {
+    artist: string
+    numberOfTracks?: number  
+    durationInMinutes?: number    
+    albumType: 'Studio Album' | 'Live Album' |
+    'Soundtrack' | 'Other'
+}
+
+function getYearsSinceRelease( mediaItem: MediaItem): number{
+    const currentYear = new Date().getFullYear()
+    return currentYear - mediaItem.year
+}
